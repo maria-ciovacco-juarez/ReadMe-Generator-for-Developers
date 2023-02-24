de4b2fdb634b4745ed8509f693eb3d73b8be253d
Maria Juarez <119270869+mariaxisabelle@users.noreply.github.com>
2023-02-24 17:07:06 -0600

    adding files to github

---
+++ index.js
@@ -0,0 +1,63 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require(".utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Project description?',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: ' Project usage?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    messsage: 'Any contribution info?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email:',
+    type: 'input',
+    name: 'github',
+    message: 'Enter your github username:',
+
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What type of license?',
+    choices: ['MIT', 'GPL 3.0', 'APACHE 2.0', 'None']
+  }
+];
+
+// generate file
+function writeFile(fileName, data){
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+  
+
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
