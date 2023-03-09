a46dd9c6a3e4ff10ce96efa72b8b95e1434ec5a1
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-03-09 15:28:43 -0600

    errors with contrubtion portion, question not generating properly

--- index.js
+++ index.js
@@ -35,31 +35,37 @@ const questions = [
     type: 'input',
     name: 'email',
     message: 'Enter your email:',
+  },
+  {
     type: 'input',
     name: 'github',
     message: 'Enter your github username:',
-
   },
   {
     type: 'input',
     name: 'license',
     message: 'What type of license?',
     choices: ['MIT', 'GPL 3.0', 'APACHE 2.0', 'None']
+  },
+  {
+    type: 'input',
+    name: 'run test',
+    message: 'What command should the user use to run tests?'
   }
 ];
 
 // generate file
-function writeFile(fileName, data){
+function writeFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 
 
 // TODO: Create a function to initialize app
-function init() { 
+function init() {
   inquirer.prompt(questions).then((responses) => {
     console.log('ReadME has been generated');
-    writeFile('README.md', generateMarkdown({...responses}));
+    writeFile('README.md', generateMarkdown({ ...responses }));
   })
 }
 
